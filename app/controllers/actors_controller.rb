51da47123ea5d4104d527af1fe183e87df4e40f5
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-01-14 22:18:26 +0000

    actor create route done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,15 @@
 class ActorsController < ApplicationController
+  def create
+    new_actor = Actor.new
+    new_actor.name = params.fetch("query_name")
+    new_actor.dob = params.fetch("query_dob")
+    new_actor.bio = params.fetch("query_bio")
+    new_actor.image = params.fetch("query_image")
+    new_actor.save
+
+    redirect_to("/actors")
+  end
+
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
