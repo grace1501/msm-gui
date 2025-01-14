1c77bc67ca7188273b0937de4e6772d6aa1de2bb
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-01-14 22:26:34 +0000

    actors routes done

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -10,6 +10,25 @@ class ActorsController < ApplicationController
     redirect_to("/actors")
   end
 
+  def destroy
+    the_id = params.fetch("path_id")
+    the_record = Actor.find(the_id)
+    the_record.destroy
+    redirect_to("/actors")
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    the_record = Actor.find(the_id)
+    the_record.name = params.fetch("query_name")
+    the_record.dob = params.fetch("query_dob")
+    the_record.bio = params.fetch("query_bio")
+    the_record.image = params.fetch("query_image")
+    the_record.save
+
+    redirect_to("/actors/#{the_id}")
+  end
+
   def index
     matching_actors = Actor.all
     @list_of_actors = matching_actors.order({ :created_at => :desc })
